db43e21f23bf55d55a079aa86075b85114b888f5
Alessandro Rodrigues <alessandjr0@gmail.com>
2024-12-11 11:44:53 -0300

    alteração app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Hello-World';
+  aluno = 'Alessandro Rodrigues de Souza Júnior';
 }
